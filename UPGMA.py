a2c8031f83b666eabaea0b1ab3efd5909d09b8d1
vrichter <vincent.richter11@outlook.de>
2019-05-16 15:34:23 +0200

    reading fasta file, put out distances between all sequences

---
+++ UPGMA.py
@@ -0,0 +1,54 @@
+
+
+
+seq1 = "AAAGGGTTT"
+
+seq2 = "AAAGGTTTT"
+
+def hammingDistance(seq1, seq2):
+    distance = 0
+    for i in range(len(seq1)):
+        if seq1[i] != seq2[i]:
+            distance += 1
+    return distance
+
+
+def read_fasta(filename):
+    """
+    Read sequences from FASTA file into dictionary, ignoring chain information.
+    :param filename: path to FASTA file
+    :return: dictionary with annotations as key
+    """
+    fasta_file = open(filename)
+    sequences = {}                         # Initialize empty dictionary
+    for line in fasta_file:
+        line = line.replace("\r","").replace("\n","")
+        if line.startswith(">"):
+            annotation = line[1:]          # Get annotation from the line after ">"
+            sequences[annotation] = ""     # Start new sequence with this annotation
+        elif line.startswith(";"):
+            pass                           # Ignore chain information after ";"
+        else:
+            sequences[annotation] += line  # Append sequence with this annotation
+    fasta_file.close()
+    return sequences
+
+
+fasta_filename = 'sequence.fasta'
+all_sequences = read_fasta(fasta_filename)
+
+for annotation, sequence in all_sequences.items():
+    for annotation2, sequence2 in all_sequences.items():
+        if annotation != annotation2:
+            print(annotation, "vs", annotation2, ':',sequence, "vs", sequence2, ":", hammingDistance(sequence, sequence2))
+        
+
+
+
+
+
+
+
+
+
+
